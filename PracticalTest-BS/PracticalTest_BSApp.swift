20c3fc2c90e2dbca07c73cbf5bafb7de7f6917bb
jubel-hossain <mdjubel31@gmail.com>
2023-09-11 20:50:36 +0600

    initial commit

---
+++ PracticalTest-BS/PracticalTest_BSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticalTest_BSApp.swift
+//  PracticalTest-BS
+//
+//  Created by Md Jubel Hossain on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct PracticalTest_BSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
