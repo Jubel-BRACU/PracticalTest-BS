8203a886d514dd2dd0c1c52493ea70ced8df5a50
jubel-hossain <mdjubel31@gmail.com>
2023-09-12 19:52:05 +0600

    MovieSearchViewModel added for searching movies

---
+++ PracticalTest-BS/Core/ViewModel/MovieSearchViewModel.swift
@@ -0,0 +1,112 @@
+//
+//  MovieSearchViewModel.swift
+//  PracticalTest-BS
+//
+//  Created by Md Jubel Hossain on 12/9/23.
+//
+
+import Combine
+import Foundation
+class MovieSearchViewModel: ObservableObject {
+    private let baseAPIURL = "https://api.themoviedb.org/3"
+    private let apiKey = "1d9b898a212ea52e283351e521e17871"
+    private let urlSession = URLSession.shared
+    @Published var movies: [Movie]?
+    @Published var searchText = ""
+    private let jsonDecoder = Utils.jsonDecoder
+    @Published var movielist = MovieList(results: [])
+    private var subscriptionToken: AnyCancellable?
+    func startSearch(){
+        self.movies = nil
+        guard subscriptionToken == nil else { return }
+        
+        self.subscriptionToken = self.$searchText
+            .map { [weak self] text in
+                self?.movies = nil
+                return text
+                
+            }.throttle(for: 1, scheduler: DispatchQueue.main, latest: true)
+            .sink { [weak self] in self?.search(query: $0) }
+        
+        
+    }
+    func search(query: String) {
+        self.fetchUsers(query: searchText.lowercased()) {[weak self] (result) in
+            switch result {
+            case .success(let response):
+                self?.movies = response.results
+                print(response.results)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    func fetchUsers(query: String, completion: @escaping (Result<MovieList, MovieError>) -> ()){
+        guard let url = URL(string: "\(baseAPIURL)/search/movie") else {
+            completion(.failure(.invalidEndpoint))
+            return
+        }
+        self.loadURLAndDecode(url: url, params: [
+            "language": "en-US",
+            "include_adult": "false",
+            "region": "US",
+            "query": query
+        ], completion: completion)
+        
+    }
+    private func loadURLAndDecode<D: Decodable>(url: URL, params: [String: String]? = nil, completion: @escaping (Result<D, MovieError>) -> ()) {
+        guard var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false) else {
+            completion(.failure(.invalidEndpoint))
+            return
+        }
+        
+        var queryItems = [URLQueryItem(name: "api_key", value: apiKey)]
+        if let params = params {
+            queryItems.append(contentsOf: params.map { URLQueryItem(name: $0.key, value: $0.value) })
+        }
+        
+        urlComponents.queryItems = queryItems
+        
+        guard let finalURL = urlComponents.url else {
+            completion(.failure(.invalidEndpoint))
+            return
+        }
+        
+        urlSession.dataTask(with: finalURL) { [weak self] (data, response, error) in
+            guard let self = self else { return }
+            
+            if error != nil {
+                self.executeCompletionHandlerInMainThread(with: .failure(.apiError), completion: completion)
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse, 200..<300 ~= httpResponse.statusCode else {
+                self.executeCompletionHandlerInMainThread(with: .failure(.invalidResponse), completion: completion)
+                return
+            }
+            
+            guard let data = data else {
+                self.executeCompletionHandlerInMainThread(with: .failure(.noData), completion: completion)
+                return
+            }
+            
+            do {
+                let decodedResponse = try self.jsonDecoder.decode(D.self, from: data)
+                self.executeCompletionHandlerInMainThread(with: .success(decodedResponse), completion: completion)
+            } catch {
+                self.executeCompletionHandlerInMainThread(with: .failure(.serializationError), completion: completion)
+            }
+        }.resume()
+    }
+    private func executeCompletionHandlerInMainThread<D: Decodable>(with result: Result<D, MovieError>, completion: @escaping (Result<D, MovieError>) -> ()) {
+        DispatchQueue.main.async {
+            completion(result)
+        }
+    }
+    
+    
+}
+
+
+
+
