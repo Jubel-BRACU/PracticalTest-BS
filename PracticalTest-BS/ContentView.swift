8005918e05e32141503a5ce7c436808e74e4cb3e
jubel-hossain <mdjubel31@gmail.com>
2023-09-12 20:10:56 +0600

    Content View UI updated

--- PracticalTest-BS/ContentView.swift
+++ PracticalTest-BS/ContentView.swift
@@ -6,23 +6,34 @@
 //
 
 import SwiftUI
-
+//MARK: - ContentView
 struct ContentView: View {
+    init() {
+        UITabBar.appearance().backgroundColor = UIColor.systemGray5
+    }
     var body: some View {
-        VStack(alignment: .leading){
-            Text("Movie List")
-                .font(.title)
-                .fontWeight(.bold)
-            Divider()
-            ScrollView{
-                VStack{
-                    ForEach(0...10, id: \.self){_ in
-                        MovieRow(movie: Movie(image: nil, title: "Captain Marvel", overview: "Captain Marvel is an extraterrestrial Kree warrior who finds herself caught in the middle of an intergalactic battle between her people and the Skrulls. Living on Earth in 1995, she keeps having recurring memories of another life as U.S. Air Force pilot Carol Danvers. With help from Nick Fury, Captain Marvel tries to uncover the secrets of her past while harnessing her special superpowers to end the war with the evil Skrulls."))
+        TabView {
+            //MARK: - MovieList View
+            MovieListView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "film")
+                        Text("Movies")
                     }
                 }
-            }
+                .tag(0)
+            
+            //MARK: - Movie Search View
+            MoviewSearchView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "magnifyingglass")
+                        Text("Search")
+                    }
+                }
+                .tag(1)
         }
-        .padding()
+        .accentColor(.green)
     }
 }
 
